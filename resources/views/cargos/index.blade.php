cec52ad1c278751f56528e9f1e10832d94c00d09
Gabriel Carlos Alves de Oliveira <43966862+GabrielDAP@users.noreply.github.com>
2022-10-20 15:33:50 -0300

    Centralizando...

--- resources/views/cargos/index.blade.php
+++ resources/views/cargos/index.blade.php
@@ -5,28 +5,30 @@
 
 @section('conteudo')
     <h1>Cargos</h1>
-        <a href="{{ route('cargos.create') }}" class="btn btn-primary float-end mb-2 rounded-circle fs-4"><i class="bi bi-person-plus-fill"></i></a>
-    <table class="table table-striped">
-        <thead class="table-dark">
-            <tr>
-                <th>ID</th>
-                <th>Descrição</th>
-                <th>Ação</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($cargos as $cargo)
-            <tr>
-                <td class="align-middle">{{ $cargo->id }}</td>
-                <td class="align-middle">{{ $cargo->descricao }}</td>
-                <td class="align-middle">
-                    <a href="#" class="btn btn-success mb-2 fs-8"><i class="bi bi-person"></i></a>
-                    <a href="#" class="btn btn-primary mb-2 fs-8"><i class="bi bi-pen"></i></a>
-                    <a href="#" class="btn btn-danger mb-2 fs-8"><i class="bi bi-trash3"></i></a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-
-    </table>
+        <div class="table-responsive">
+            <a href="{{ route('cargos.create') }}" class="btn btn-primary float-end mb-2 rounded-circle fs-4"><i class="bi bi-person-plus-fill"></i></a>
+            <table class="table table-striped">
+                <thead class="table-dark">
+                    <tr class="text-center">
+                        <th>ID</th>
+                        <th>Descrição</th>
+                        <th width = "160">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($cargos as $cargo)
+                    <tr class="text-center">
+                        <td class="align-middle">{{ $cargo->id }}</td>
+                        <td class="align-middle">{{ $cargo->descricao }}</td>
+                        <td class="align-middle">
+                            <a href="#" class="btn btn-success mb-2 fs-8"><i class="bi bi-person"></i></a>
+                            <a href="#" class="btn btn-primary mb-2 fs-8"><i class="bi bi-pen"></i></a>
+                            <a href="#" class="btn btn-danger mb-2 fs-8"><i class="bi bi-trash3"></i></a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+        
+            </table>
+        </div>
 @endsection
--- resources/views/departamentos/index.blade.php
+++ resources/views/departamentos/index.blade.php
@@ -5,28 +5,30 @@
 
 @section('conteudo')
     <h1>Departamentos</h1>
-        <a href="{{ route('departamentos.create') }}" class="btn btn-primary float-end mb-2 rounded-circle fs-4"><i class="bi bi-person-plus-fill"></i></a>
-    <table class="table table-striped">
-        <thead class="table-dark">
-            <tr>
-                <th>ID</th>
-                <th>Descrição</th>
-                <th>Ação</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($departamentos as $departamento)
-            <tr>
-                <td class="align-middle">{{ $departamento->id }}</td>
-                <td class="align-middle">{{ $departamento->nome }}</td>
-                <td class="align-middle">
-                    <a href="#" class="btn btn-success mb-2 fs-8"><i class="bi bi-person"></i></a>
-                    <a href="#" class="btn btn-primary mb-2 fs-8"><i class="bi bi-pen"></i></a>
-                    <a href="#" class="btn btn-danger mb-2 fs-8"><i class="bi bi-trash3"></i></a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-
-    </table>
+        <div class="table-responsive">
+            <a href="{{ route('departamentos.create') }}" class="btn btn-primary float-end mb-2 rounded-circle fs-4"><i class="bi bi-person-plus-fill"></i></a>
+            <table class="table table-striped">
+                <thead class="table-dark">
+                    <tr class="text-center">
+                        <th>ID</th>
+                        <th>Descrição</th>
+                        <th width = "160">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($departamentos as $departamento)
+                    <tr class="text-center">
+                        <td class="align-middle">{{ $departamento->id }}</td>
+                        <td class="align-middle">{{ $departamento->nome }}</td>
+                        <td class="align-middle">
+                            <a href="#" class="btn btn-success mb-2 fs-8"><i class="bi bi-person"></i></a>
+                            <a href="#" class="btn btn-primary mb-2 fs-8"><i class="bi bi-pen"></i></a>
+                            <a href="#" class="btn btn-danger mb-2 fs-8"><i class="bi bi-trash3"></i></a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+        
+            </table>
+        </div>
 @endsection
